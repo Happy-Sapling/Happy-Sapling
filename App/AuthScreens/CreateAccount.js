8ac11a9f635c564cd35370cb5e10a2f472f52d18
Jeannie Q <jeanniequach01@gmail.com>
2021-06-19 15:24:14 -0400

    Update CreateAccount.js

--- App/AuthScreens/CreateAccount.js
+++ App/AuthScreens/CreateAccount.js
@@ -1,180 +1,78 @@
-/*import React from "react";
+import * as React from "react";
 import {
   Text,
   View,
   StyleSheet,
   Dimensions,
   TouchableOpacity,
+  TextInput,
 } from "react-native";
 
 import { AuthContext } from "../context";
 
-import {
-  useFonts,
-  Lato_300Light,
-  Lato_400Regular,
-  Lato_700Bold,
-  Lato_900Black,
-} from "@expo-google-fonts/lato";
-import AppLoading from "expo-app-loading";
+// or any pure javascript modules available in npm
 
 const { width, height } = Dimensions.get("screen");
 
 export default function CreateAccount({ navigation }) {
   //Please do not take out the following:
   const { signUp } = React.useContext(AuthContext);
-
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Create Account Screen</Text>
-      <View style={{ top: 150 }}>
-        <TouchableOpacity style={styles.SignIn} onPress={() => signUp()}>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: "center",
-              backgroundColor: "transparent",
-            }}
-          >
-            <Text style={styles.SignInText}>Create your account</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-}
+      <Text style={styles.title}>Happy Sapling</Text>
 
-const styles = StyleSheet.create({
-  container: {
-    height: height,
-    width: width,
-    justifyContent: "center",
-    alignItems: "center",
-    flex: 1,
-    backgroundColor: "white",
-  },
-  title: {
-    marginTop: "-40%",
-    fontSize: 24,
-    textAlign: "center",
-  },
-
-  inputBox: {
-    backgroundColor: "white",
-    width: width * 0.7,
-    marginTop: 50,
-  },
-  header: {
-    fontSize: 16,
-  },
-  inputContainer: {
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  signInput: {
-    backgroundColor: "rgba(242,204,143,0.67)",
-    fontSize: 22,
-    height: 40,
-    borderRadius: 8,
-  },
-  passwordInput: {
-    backgroundColor: "rgba(224,122,95,0.21)",
-    fontSize: 22,
-    borderRadius: 8,
-    height: 40,
-  },
-  SignIn: {
-    padding: 15,
-    marginTop: 70,
-    width: width * 0.7,
-    height: 50,
-    alignSelf: "center",
-    borderRadius: 8,
-    backgroundColor: "rgba(224,122,95,0.58)",
-  },
-  SignInText: {
-    textAlign: "center",
-    fontSize: 22,
-  },
-  createAccountSection: {
-    flexDirection: "row",
-    justifyContent: "center",
-    marginTop: 15,
-  },
-});
-*/
-
-import * as React from 'react';
-import { Text, View, StyleSheet, Dimensions, TouchableOpacity, TextInput } from "react-native";
-import Constants from 'expo-constants';
-
-// You can import from local files
-import AssetExample from './components/AssetExample';
-
-// or any pure javascript modules available in npm
-
-const { width, height } = Dimensions.get("screen");
-
-export default function App() {
-  return (
-   <View style={styles.container}>
-
-        <Text style={styles.title}>Happy Sapling</Text>
-
-        <View style={styles.inputBox}>
-          <Text style={styles.header}>First Name</Text>
-          <View style={styles.inputContainer}>
-            <TextInput
-              blurOnSubmit={false}
-              returnKeyType="next"
-              style={styles.firstNameInput}
-            />
-          </View>
-
-          <Text style={styles.header}>Last Name</Text>
-          <View style={styles.inputContainer}>
-            <TextInput
-              blurOnSubmit={false}
-              returnKeyType="next"
-              style={styles.lastNameInput}
-            />
-          </View>
-
-          <Text style={styles.header}>Email</Text>
-          <View style={styles.inputContainer}>
-            <TextInput
-              blurOnSubmit={false}
-              returnKeyType="done"
-              style={styles.emailInput}
-            />
-          </View>
-
-          <Text style={styles.header}>Password</Text>
-          <View style={styles.inputContainer}>
-            <TextInput
-              blurOnSubmit={false}
-              returnKeyType="done"
-              style={styles.passwordInput}
-            />
-          </View>
-
-          <Text style={styles.header}>Confirm Password</Text>
-          <View style={styles.inputContainer}>
-            <TextInput
-              blurOnSubmit={false}
-              returnKeyType="done"
-              style={styles.confirmPasswordInput}
-            />
-          </View>
-
+      <View style={styles.inputBox}>
+        <Text style={styles.header}>First Name</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            blurOnSubmit={false}
+            returnKeyType="next"
+            style={styles.firstNameInput}
+          />
         </View>
 
-        <TouchableOpacity style={styles.createAccount}>
-              <Text style={styles.createAccountText}>Create Account</Text>
-        </TouchableOpacity>
+        <Text style={styles.header}>Last Name</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            blurOnSubmit={false}
+            returnKeyType="next"
+            style={styles.lastNameInput}
+          />
+        </View>
 
+        <Text style={styles.header}>Email</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            blurOnSubmit={false}
+            returnKeyType="done"
+            style={styles.emailInput}
+          />
+        </View>
+
+        <Text style={styles.header}>Password</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            blurOnSubmit={false}
+            returnKeyType="done"
+            style={styles.passwordInput}
+          />
+        </View>
+
+        <Text style={styles.header}>Confirm Password</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            blurOnSubmit={false}
+            returnKeyType="done"
+            style={styles.confirmPasswordInput}
+          />
+        </View>
       </View>
-    );
+
+      <TouchableOpacity style={styles.createAccount} onPress={() => signUp()}>
+        <Text style={styles.createAccountText}>Create Account</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -198,7 +96,7 @@ const styles = StyleSheet.create({
     marginTop: 10, // 50
     top: "9%",
     flex: 1,
-    flexDirection: 'column'
+    flexDirection: "column",
   },
   header: {
     fontSize: 16,
@@ -251,6 +149,6 @@ const styles = StyleSheet.create({
   createAccountText: {
     textAlign: "center",
     fontSize: 18,
-    color: "white"
-  }
+    color: "white",
+  },
 });
