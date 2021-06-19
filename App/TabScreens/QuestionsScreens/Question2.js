a2b6371c43f0fba3c2f614270e1c15fba6a67fee
JF236 <jadeforrest1@outlook.com>
2021-06-18 21:08:58 -0700

    adding files

--- App/TabScreens/QuestionsScreens/Question2.js
+++ App/TabScreens/QuestionsScreens/Question2.js
@@ -1,4 +1,4 @@
-import React from "react";
+/* import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
 export default function Question2({ navigation }) {
@@ -25,4 +25,88 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     borderRadius: 5,
   },
+});
+ */
+
+import * as React from 'react';
+import { 
+  Text,
+  View, 
+  StyleSheet,
+  TextInput,
+  Dimensions,
+  TouchableOpacity
+  } from 'react-native';
+
+const {width, height} = Dimensions.get("screen")
+
+
+
+export default function Question2({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>How are you</Text>
+      <Text style={styles.title}>feeling today?</Text>
+
+      <View style={styles.box}>
+        <View style={styles.submitButton}>
+          <TouchableOpacity  onPress={() => navigation.push("Question3")}>
+            <Text style={styles.textSubmit}>Submit Entry</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View>
+        <Text/>
+        <TextInput style={styles.words}
+          bluronSubmit={false}
+          placeholder="Insert Text Here"
+          placeholderTextColor="#474747"/>
+      </View>
+      
+    </View>
+
+    
+
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#f5f5f5',
+    padding: 8,
+  },
+  title: {
+    flexDirection:"row",
+    textAlign: 'center',
+    fontSize: 30,
+  },
+  box:{
+    backgroundColor:"#CCE1F2",
+    height: height *0.56,
+    borderRadius:20,
+    marginHorizontal: width *0.07,
+    marginTop:height*.04
+  },
+  submitButton:{
+    marginLeft: width * .472,
+    marginTop: height * .46,
+    width: width*.3,
+    height:height *0.07,
+    borderRadius:20,
+    backgroundColor: "#878AA0",
+  },
+  textSubmit:{
+    textAlign:"center",
+    color:"#F4F1DE",
+    paddingVertical:height *.022
+  },
+  words:{
+    marginTop:-height * .54,
+    marginHorizontal: width *0.12,
+
+  }
 });
