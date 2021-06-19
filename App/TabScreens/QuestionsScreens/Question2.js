bb1122f5bc9c964a6720304362aec24f6223637d
Jeannie Q <jeanniequach01@gmail.com>
2021-06-19 00:14:40 -0400

    Removing header + changing top padding

--- App/TabScreens/QuestionsScreens/Question2.js
+++ App/TabScreens/QuestionsScreens/Question2.js
@@ -28,19 +28,17 @@ const styles = StyleSheet.create({
 });
  */
 
-import * as React from 'react';
-import { 
+import * as React from "react";
+import {
   Text,
-  View, 
+  View,
   StyleSheet,
   TextInput,
   Dimensions,
-  TouchableOpacity
-  } from 'react-native';
+  TouchableOpacity,
+} from "react-native";
 
-const {width, height} = Dimensions.get("screen")
-
-
+const { width, height } = Dimensions.get("screen");
 
 export default function Question2({ navigation }) {
   return (
@@ -50,63 +48,59 @@ export default function Question2({ navigation }) {
 
       <View style={styles.box}>
         <View style={styles.submitButton}>
-          <TouchableOpacity  onPress={() => navigation.push("Question3")}>
+          <TouchableOpacity onPress={() => navigation.push("Question3")}>
             <Text style={styles.textSubmit}>Submit Entry</Text>
           </TouchableOpacity>
         </View>
       </View>
       <View>
-        <Text/>
-        <TextInput style={styles.words}
+        <Text />
+        <TextInput
+          style={styles.words}
           bluronSubmit={false}
           placeholder="Insert Text Here"
-          placeholderTextColor="#474747"/>
+          placeholderTextColor="#474747"
+        />
       </View>
-      
     </View>
-
-    
-
-
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#f5f5f5',
+    justifyContent: "center",
+    paddingTop: "10%",
+    backgroundColor: "#f5f5f5",
     padding: 8,
   },
   title: {
-    flexDirection:"row",
-    textAlign: 'center',
+    flexDirection: "row",
+    textAlign: "center",
     fontSize: 30,
   },
-  box:{
-    backgroundColor:"#CCE1F2",
-    height: height *0.56,
-    borderRadius:20,
-    marginHorizontal: width *0.07,
-    marginTop:height*.04
+  box: {
+    backgroundColor: "#CCE1F2",
+    height: height * 0.56,
+    borderRadius: 20,
+    marginHorizontal: width * 0.07,
+    marginTop: height * 0.04,
   },
-  submitButton:{
-    marginLeft: width * .472,
-    marginTop: height * .46,
-    width: width*.3,
-    height:height *0.07,
-    borderRadius:20,
+  submitButton: {
+    marginLeft: width * 0.472,
+    marginTop: height * 0.46,
+    width: width * 0.3,
+    height: height * 0.07,
+    borderRadius: 20,
     backgroundColor: "#878AA0",
   },
-  textSubmit:{
-    textAlign:"center",
-    color:"#F4F1DE",
-    paddingVertical:height *.022
+  textSubmit: {
+    textAlign: "center",
+    color: "#F4F1DE",
+    paddingVertical: height * 0.022,
   },
-  words:{
-    marginTop:-height * .54,
-    marginHorizontal: width *0.12,
-
-  }
+  words: {
+    marginTop: -height * 0.54,
+    marginHorizontal: width * 0.12,
+  },
 });
