2bc10e3738be89b8faecdb4809d95b058a4bdf5e
juishah14 <juishah14@gmail.com>
2021-06-19 20:50:27 -0400

    Fixed all remaining font issues
    Fixed all remaining font issues

--- App/TabScreens/Profile.js
+++ App/TabScreens/Profile.js
@@ -4,16 +4,21 @@ import { Text, View, StyleSheet, Dimensions, Image } from 'react-native';
 
 import {
   useFonts,
-  Lato_300Light,
   Lato_400Regular,
-  Lato_700Bold,
-  Lato_900Black,
 } from "@expo-google-fonts/lato";
+
+import AppLoading from "expo-app-loading";
 
 const {width, height} = Dimensions.get("screen")
 
 export default function Profile({ navigation }) {
   const { signOut } = React.useContext(AuthContext);
+  let [fontsLoaded] = useFonts({
+    Lato_400Regular
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
   return (
     <View style={styles.container}>
 
@@ -55,7 +60,7 @@ export default function Profile({ navigation }) {
 
     </View>
   );
-}
+}}
 
 const styles = StyleSheet.create({
   container: {
